13f326700bbd5e3b49f26d66a182cdfbc3d29cd1
ayungavis <ayungavis@gmail.com>
2021-08-11 12:30:35 +0700

    🎨  style: make background to white

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,6 +14,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whiteColor,
       body: SafeArea(
         bottom: false,
         child: ListView(
